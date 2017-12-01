5eae4fcb61d589e03c92157c76fc0cc8e2a04ece
Roger Arques <rarquesvall@gmail.com>
2017-12-01 13:28:25 +0100

    Added routing btw navbar and search-by-transcript. Adjusted main jumbotron

--- src/app.routing.ts
+++ src/app.routing.ts
@@ -1,9 +1,11 @@
 import {AboutComponent} from './app/about/about.component';
 import {Routes} from '@angular/router';
 import {WelcomeComponent} from './app/welcome/welcome.component';
+import {SearchByTranscriptComponent} from './app/sequence/search-by-transcript/search-by-transcript.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'welcome', pathMatch: 'full'},
   {path: 'welcome', component: WelcomeComponent},
-  {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent},
+  {path: 'search/search-by-transcript', component: SearchByTranscriptComponent}
 ];
