688e194b83db1fa364e2bb65735f3231fa2a1172
Roger Arques <rarquesvall@gmail.com>
2017-12-01 01:28:40 +0100

    Added welcome page to routing and changed styles a bit

--- src/app.routing.ts
+++ src/app.routing.ts
@@ -1,7 +1,9 @@
 import {AboutComponent} from './app/about/about.component';
 import {Routes} from '@angular/router';
+import {WelcomeComponent} from './app/welcome/welcome.component';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'about', pathMatch: 'full'},
+  {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+  {path: 'welcome', component: WelcomeComponent},
   {path: 'about', component: AboutComponent}
 ];
