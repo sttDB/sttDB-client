2c90046d01a15260de3831caba8fbe89c5a274ff
Roger Arques <rarquesvall@gmail.com>
2017-12-04 16:13:01 +0100

    Created template to upload files and added FileUploader to component

--- src/app/fasta-upload/fasta-upload.component.ts
+++ src/app/fasta-upload/fasta-upload.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FileUploader} from 'ng2-file-upload';
+import {environment} from '../../environments/environment.prod';
 
 @Component({
   selector: 'app-fasta-upload',
   templateUrl: './fasta-upload.component.html',
-  styleUrls: ['./fasta-upload.component.css']
+  styleUrls: ['./fasta-upload.component.css'],
 })
 export class FastaUploadComponent implements OnInit {
 
   fastaForm: FormGroup;
+  uploader = new FileUploader({url: `${environment.API}/uploadFasta`});
 
   constructor(private fb: FormBuilder,
               private router: Router) {
