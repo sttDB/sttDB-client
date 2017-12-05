edf4ae8405a6d6b8878095c4d0e65e6a6aff2ac7
Nector12 <rarquesvall@gmail.com>
2017-12-05 18:42:23 +0100

    Moved all search tools into search folder

--- src/app.routing.ts
+++ src/app.routing.ts
@@ -1,9 +1,9 @@
 import {AboutComponent} from './app/about/about.component';
 import {Routes} from '@angular/router';
 import {WelcomeComponent} from './app/welcome/welcome.component';
-import {SearchByTranscriptComponent} from './app/sequence/search-by-transcript/search-by-transcript.component';
+import {SearchByTranscriptComponent} from './app/sequence/search/search-by-transcript/search-by-transcript.component';
 import {FastaUploadComponent} from './app/fasta-upload/fasta-upload.component';
-import {SearchByTrinityIdComponent} from './app/sequence/search-by-trinity-id/search-by-trinity-id.component';
+import {SearchByTrinityIdComponent} from './app/sequence/search/search-by-trinity-id/search-by-trinity-id.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'welcome', pathMatch: 'full'},
