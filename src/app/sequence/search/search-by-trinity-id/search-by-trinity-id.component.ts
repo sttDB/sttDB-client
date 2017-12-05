e9758a01b08d98c3d04f45d3a998fe95390ea3bb
Nector12 <rarquesvall@gmail.com>
2017-12-05 19:23:11 +0100

    Created form in search component to query sequences Imported FormsModule to make it compile

--- src/app/sequence/search/search-by-trinity-id/search-by-trinity-id.component.ts
+++ src/app/sequence/search/search-by-trinity-id/search-by-trinity-id.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-search-by-trinity-id',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchByTrinityIdComponent implements OnInit {
 
-  constructor() { }
+  public trinityId: string;
+  public trinityForm: FormGroup;
+  public errorMessage: string;
+
+  constructor(private fb: FormBuilder,
+              private router: Router) {
+    this.trinityForm = fb.group({
+      'trinity-id': ['Administrator username']
+    });
+  }
 
   ngOnInit() {
+
+  }
+
+  onSubmit(): void {
+    console.log("submit!" + this.trinityId)
   }
 
 }
