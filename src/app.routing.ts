a869ba54df093f88e4c0f93043d9881765abf245
Nector12 <rarquesvall@gmail.com>
2017-12-05 18:39:49 +0100

    Added routes to routing

--- src/app.routing.ts
+++ src/app.routing.ts
@@ -3,11 +3,13 @@ import {Routes} from '@angular/router';
 import {WelcomeComponent} from './app/welcome/welcome.component';
 import {SearchByTranscriptComponent} from './app/sequence/search-by-transcript/search-by-transcript.component';
 import {FastaUploadComponent} from './app/fasta-upload/fasta-upload.component';
+import {SearchByTrinityIdComponent} from './app/sequence/search-by-trinity-id/search-by-trinity-id.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'welcome', pathMatch: 'full'},
   {path: 'welcome', component: WelcomeComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'search/search-by-id', component: SearchByTrinityIdComponent},
   {path: 'search/search-by-transcript', component: SearchByTranscriptComponent},
   {path: 'upload', component: FastaUploadComponent}
 ];
