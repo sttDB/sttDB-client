1339ccb416a1a3703174935c0306eaa551c79777
Marc Ribalta Gene <mariva1995@gmail.com>
2017-12-07 01:26:27 +0100

    added new http methods

--- src/app/sequence/sequence.service.ts
+++ src/app/sequence/sequence.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import {Observable} from 'rxjs/Observable';
+import {Sequence} from './sequence';
+import {environment} from '../../environments/environment';
 
 @Injectable()
 export class SequenceService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  // GET /sequences /trinityId
+  getSequencesByTrinityId(trinityId: string): Observable<Sequence[]> {
+    return this.http.get(`${environment.API}/sequences/search/findByTrinityId/${trinityId}`)
+      .map((res: Response) => new Sequence(res.json()))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
+
+  // GET /sequences /transcript
+  getSequencesByTranscript(transcript: string): Observable<Sequence[]> {
+    return this.http.get(`${environment.API}/sequences/search/findByTranscript/${transcript}`)
+      .map((res: Response) => new Sequence(res.json()))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
 
 }
