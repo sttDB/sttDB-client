0319764b0a374d86ee37a7f03f0364098cc91cee
Marc Ribalta Gene <mariva1995@gmail.com>
2018-01-15 20:06:08 +0100

    added index for the page

--- src/app/pager/page.ts
+++ src/app/pager/page.ts
@@ -2,6 +2,7 @@ export class Page {
   listOfElements: any = [];
   numberOfElements: number;
   numberOfPages: number;
+  pageIndex: number;
 
   constructor(values: Object = {}){
     Object.assign(this, values);
