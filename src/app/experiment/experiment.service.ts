bd5538a90e6d79dd8be537d3502f831ba429dab6
Marc Ribalta Gene <mariva1995@gmail.com>
2018-01-22 17:20:53 +0100

    Getting files and listing them in a tab

--- src/app/experiment/experiment.service.ts
+++ src/app/experiment/experiment.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import {Experiment} from "./experiment";
+import {environment} from '../../environments/environment';
+import {Http, Response} from "@angular/http";
 
 @Injectable()
 export class ExperimentService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  // GET Experiments
+  getExperiments(): Observable<Experiment[]> {
+    return this.http.get(`${environment.API}/experiments`)
+      .map((res: Response) => res.json()._embedded.experiments.map(json => new Experiment(json)))
+      .catch((error: any) => Observable.throw(error.json()));
+  }
 }
--- src/app/file-downloader/file-downloader.component.ts
+++ src/app/file-downloader/file-downloader.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Experiment} from "../experiment/experiment";
+import {ExperimentService} from "../experiment/experiment.service";
 
 @Component({
   selector: 'app-file-downloader',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./file-downloader.component.css']
 })
 export class FileDownloaderComponent implements OnInit {
+  experiments: Experiment[];
+  errorMessage = '';
 
-  constructor() { }
+  constructor(private experimentService: ExperimentService) { }
 
   ngOnInit() {
+    this.experimentService.getExperiments().subscribe(
+      (exp: Experiment[]) => {
+        this.experiments = exp;
+      },
+        error => this.errorMessage = <any>error.message);
+  }
+
+  chargeExperimentFiles() {
+
   }
 
 }
