72db172729a04584054254020afa7faf75e69bf0
Marc Ribalta Gene <mariva1995@gmail.com>
2018-01-23 22:03:20 +0100

    done the download client part, working correctly

--- src/app/file-downloader/file-downloader.service.ts
+++ src/app/file-downloader/file-downloader.service.ts
@@ -10,16 +10,30 @@ export class FileDownloaderService {
   constructor(private http: Http) {
   }
 
-  createFasta(id: String, experiment: String) {
-    const type = 'application/vnd.ms-excel';
-    const options = new RequestOptions({
-      responseType: ResponseContentType.Blob,
-      headers: new Headers({ 'Accept': type })
-    });
+  createFasta(id: string, experiment: string) {
+    const options = this.prepareOptions();
     this.http.get(`${environment.API}/download/fasta?trinityId=${id}&experiment=${experiment}`, options)
       .subscribe(data => {
         window.open(data.url);
         },
         error => console.log(error));
+  }
+
+  downloadExperimentFile(experiment: string, name: string) {
+    const options = this.prepareOptions();
+    this.http.get(`${environment.API}/download/${experiment}?file=${name}`, options)
+      .subscribe(data => {
+          window.open(data.url);
+        },
+        error => console.log(error));
+  }
+
+  private prepareOptions() {
+    const type = 'application/vnd.ms-excel';
+    const options = new RequestOptions({
+      responseType: ResponseContentType.Blob,
+      headers: new Headers({'Accept': type})
+    });
+    return options;
   }
 }
