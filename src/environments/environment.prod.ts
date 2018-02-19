da91cc0c280c3b7312632ccad2f6f492ef9433a0
Roger Arqués <rarquesvall@gmail.com>
2018-02-19 15:25:35 +0100

    Added production server urls to env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API: 'http://localhost:8080',
-  BLAST_URL: 'http://localhost:4567'
+  API: 'http://sttdb.udl.cat:8080',
+  BLAST_URL: 'http://sttdb.udl.cat:4567'
 };
