1bf1d0a756797ffeee37f31f87ab838af04137f5
Marc Ribalta Gene <mariva1995@gmail.com>
2018-04-13 13:56:20 +0200

    query working

--- src/app/keyword-listing/keyword-listing.component.ts
+++ src/app/keyword-listing/keyword-listing.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {Page} from "../pager/page";
-import {FamilyService} from "../family/family.service";
 import {KeywordService} from "./keyword.service";
 
 @Component({
@@ -11,7 +10,7 @@ import {KeywordService} from "./keyword.service";
 })
 export class KeywordListingComponent implements OnInit {
 
-
+  query = {params: [], sign: []};
   serviceCallType: any;
   public keyword: string;
   public preListForm: FormGroup;
@@ -31,29 +30,67 @@ export class KeywordListingComponent implements OnInit {
 
   }
 
-  /**
-   * Idealment seria:
-   * decideServiceMethod
-   * useMethodListedFilers
-   * showResult
-   */
-
   onSubmit() {
     this.edited = false;
     this.serviceCallType = this.decideServiceCall();
     this.getKeywordInfo();
   }
 
+  //TO DO: refactor this shitty method
   private decideServiceCall() {
-    /*This should be refactored into a class*/
+    this.query = {params: [], sign: []};
+    /*This should be refactored into a class TO DO*/
     if(this.keyword.search(" AND | and | or | OR | NOT | not ") == -1){
+      this.query.params.push(this.keyword);
       return this.keywordService.combinations.simple;
+    }
+    // parse the two lines
+    let outerCondition = this.keyword.split(" ").join("");
+    let innerConditionStart = outerCondition.search("\\(");
+    let innerConditionEnd = outerCondition.search("\\)");
+    let innerCondition = outerCondition.substring(innerConditionStart, innerConditionEnd+1);
+    outerCondition = outerCondition.replace(innerCondition, ""); // outter parentheses
+    innerCondition = innerCondition.substring(innerConditionStart+1, innerConditionEnd); // inner parentheses
+    let s;
+    //find first condition
+    if(innerCondition.search("AND|and") != -1){
+      this.query.sign.push("AND");
+      s = innerCondition.split(/AND|and/);
+    }else if(innerCondition.search("OR|or") != -1){
+      this.query.sign.push("OR");
+      s = innerCondition.split(/OR|or/);
+    }else if(innerCondition.search("NOT|not") != -1){
+      this.query.sign.push("NOT");
+      s = innerCondition.split(/NOT|not/);
+    }
+    this.query.params.push(s[0]);
+    this.query.params.push(s[1]);
+    let x;
+    //find second condition
+    if(outerCondition.search("AND|and") != -1){
+      this.query.sign.push("AND");
+      x = outerCondition.split(/AND|and/).join("");
+    }else if(outerCondition.search("OR|or") != -1){
+      this.query.sign.push("OR");
+      x = outerCondition.split(/OR|or/).join("");
+    }else if(outerCondition.search("NOT|not") != -1){
+      this.query.sign.push("NOT");
+      x = outerCondition.split(/NOT|not/).join("");
+    }
+    this.query.params.push(x);
+    //find method to be returned
+    if(this.query.sign.length >= 2){
+      console.log(this.query.sign[0] + " " + this.query.sign[1]);
+      console.log(this.query.params);
+      return this.keywordService.combinations[this.query.sign[0] + " " + this.query.sign[1]];
+    }else{
+      console.log("b");
+      return this.keywordService.combinations[this.query.sign[0]];
     }
   }
 
   private getKeywordInfo() {
-    console.log(this.serviceCallType);
-    this.serviceCallType("families", this.keyword, 0)
+    this.serviceCallType("families", this.query.params, 0)
       .subscribe(
         (page: Page) => {
           this.page = page;
@@ -70,7 +107,7 @@ export class KeywordListingComponent implements OnInit {
     } else if (wantedPage > this.page.totalPages - 1) {
       wantedPage = this.page.totalPages;
     }
-    this.keywordService.getEntityByDescription("family", this.keyword, wantedPage - 1)
+    this.keywordService.getEntityByDescription("families", this.query.params, wantedPage - 1)
       .subscribe(
         (page: Page) => {
           this.page = page;
--- src/app/keyword-listing/keyword.service.ts
+++ src/app/keyword-listing/keyword.service.ts
@@ -12,81 +12,77 @@ export class KeywordService {
 
   /*AND|OR|NOT, AND OR, OR AND, AND NOT, OR NOT, NOT AND, NOT OR*/
   public combinations = {
-    "simple": (entity, keyword, pageNumber) => this.getEntityByDescription(entity, keyword, pageNumber),
-    "AND": (entity, firstKeyword, secondKeyword, pageNumber) => this.getEntityByDescriptionLikeAndLike(entity, firstKeyword, secondKeyword, pageNumber),
-    "OR": (entity, orKeyword, otherOrKeyword, pageNumber) => this.getEntityByOrKeyword(entity, orKeyword, otherOrKeyword, pageNumber),
-    "NOT": (entity, keyword, notKeyword, pageNumber) => this.getEntityByDescriptionLikeAndNotLike(entity, keyword, notKeyword, pageNumber),
-    "AND NOT": (entity, firstKeyword, secondKeyword, notKeyword, pageNumber) => this.getEntityByDescriptionLikeAndLikeAndNotLike(entity, firstKeyword, secondKeyword, notKeyword, pageNumber),
-    "AND OR": (entity, firstKeyword, secondKeyword, otherKeyword, pageNumber) => this.getEntityByDescriptionLikeAndLikeOrLike(entity, firstKeyword, secondKeyword, otherKeyword, pageNumber),
-    "OR NOT": (entity, orKeyword, otherOrKeyword, notKeyword, pageNumber) => this.getEntityByAnyKeywordNotOther(entity, orKeyword, otherOrKeyword, notKeyword, pageNumber),
-    "OR AND": (entity, orKeyword, otherOrKeyword, andKeyword, pageNumber) => this.getEntityByAnyKeywordAndOther(entity, orKeyword, otherOrKeyword, andKeyword, pageNumber)
+    "simple": (entity, params, pageNumber) => this.getEntityByDescription(entity, params, pageNumber),
+    "AND": (entity, params, pageNumber) => this.getEntityByDescriptionLikeAndLike(entity, params, pageNumber),
+    "OR": (entity, params, pageNumber) => this.getEntityByOrKeyword(entity, params, pageNumber),
+    "NOT": (entity, params, pageNumber) => this.getEntityByDescriptionLikeAndNotLike(entity, params, pageNumber),
+    "AND NOT": (entity, params, pageNumber) => this.getEntityByDescriptionLikeAndLikeAndNotLike(entity, params, pageNumber),
+    "AND OR": (entity, params, pageNumber) => this.getEntityByDescriptionLikeAndLikeOrLike(entity, params, pageNumber),
+    "OR NOT": (entity, params, pageNumber) => this.getEntityByAnyKeywordNotOther(entity, params, pageNumber),
+    "OR AND": (entity, params, pageNumber) => this.getEntityByAnyKeywordAndOther(entity, params, pageNumber)
   };
 
   constructor(private http: Http) {
   }
 
-  getEntityByDescription(entity: string, keyword: string, pageNumber: number): Observable<Page> {
-    return this.http.get(`${environment.API}/${entity}?descriptionKeyword=${keyword}&page=${pageNumber}`)
+  getEntityByDescription(entity: string, params: string[], pageNumber: number): Observable<Page> {
+    return this.http.get(`${environment.API}/${entity}?descriptionKeyword=${params[0]}&page=${pageNumber}`)
       .map((res: Response) => {
         return new Page(res.json());
       })
       .catch((error: any) => Observable.throw(error.json()));
   }
 
-  getEntityByDescriptionLikeAndLike(entity: string, firstKeyword: string, secondKeyword: string, pageNumber: number): Observable<Page> {
-    return this.http.get(`${environment.API}/${entity}?firstKeyword=${firstKeyword}&secondKeyword=${secondKeyword}&page=${pageNumber}`)
+  getEntityByDescriptionLikeAndLike(entity: string, params: string[], pageNumber: number): Observable<Page> {
+    return this.http.get(`${environment.API}/${entity}?firstKeyword=${params[0]}&secondKeyword=${params[1]}&page=${pageNumber}`)
       .map((res: Response) => {
         return new Page(res.json());
       })
       .catch((error: any) => Observable.throw(error.json()));
   }
 
-  getEntityByOrKeyword(entity: string, orKeyword: string, otherOrKeyword: string, pageNumber: number): Observable<Page> {
-    return this.http.get(`${environment.API}/${entity}?orKeyword=${orKeyword}&otherOrKeyword=${otherOrKeyword}&page=${pageNumber}`)
+  getEntityByOrKeyword(entity: string, params: string[], pageNumber: number): Observable<Page> {
+    return this.http.get(`${environment.API}/${entity}?orKeyword=${params[0]}&otherOrKeyword=${params[1]}&page=${pageNumber}`)
       .map((res: Response) => {
         return new Page(res.json());
       })
       .catch((error: any) => Observable.throw(error.json()));
   }
 
-  getEntityByDescriptionLikeAndNotLike(entity: string, keyword: string, notKeyword: string, pageNumber: number): Observable<Page> {
-    return this.http.get(`${environment.API}/${entity}?keyword=${keyword}&notKeyword=${notKeyword}&page=${pageNumber}`)
+  getEntityByDescriptionLikeAndNotLike(entity: string, params: string[], pageNumber: number): Observable<Page> {
+    return this.http.get(`${environment.API}/${entity}?keyword=${params[0]}&notKeyword=${params[1]}&page=${pageNumber}`)
       .map((res: Response) => {
         return new Page(res.json());
       })
       .catch((error: any) => Observable.throw(error.json()));
   }
 
-  getEntityByDescriptionLikeAndLikeAndNotLike(entity: string, firstKeyword: string, secondKeyword: string,
-                                              notKeyword: string, pageNumber: number): Observable<Page> {
-    return this.http.get(`${environment.API}/${entity}?firstKeyword=${firstKeyword}&secondKeyword=${secondKeyword}&notKeyword=${notKeyword}&page=${pageNumber}`)
+  getEntityByDescriptionLikeAndLikeAndNotLike(entity: string, params: string[], pageNumber: number): Observable<Page> {
+    return this.http.get(`${environment.API}/${entity}?firstKeyword=${params[0]}&secondKeyword=${params[1]}&notKeyword=${params[2]}&page=${pageNumber}`)
       .map((res: Response) => {
         return new Page(res.json());
       })
       .catch((error: any) => Observable.throw(error.json()));
   }
 
-  getEntityByDescriptionLikeAndLikeOrLike(entity: string, firstKeyword: string, secondKeyword: string,
-                                          otherKeyword: string, pageNumber: number): Observable<Page> {
-    return this.http.get(`${environment.API}/${entity}?firstKeyword=${firstKeyword}&secondKeyword=${secondKeyword}&otherKeyword=${otherKeyword}&page=${pageNumber}`)
+  getEntityByDescriptionLikeAndLikeOrLike(entity: string, params: string[], pageNumber: number): Observable<Page> {
+    return this.http.get(`${environment.API}/${entity}?firstKeyword=${params[0]}&secondKeyword=${params[1]}&otherKeyword=${params[2]}&page=${pageNumber}`)
       .map((res: Response) => {
         return new Page(res.json());
       })
       .catch((error: any) => Observable.throw(error.json()));
   }
 
-  getEntityByAnyKeywordNotOther(entity: string, orKeyword: string, otherOrKeyword: string, notKeyword: string,
-                                pageNumber: number): Observable<Page> {
-    return this.http.get(`${environment.API}/${entity}?orKeyword=${orKeyword}&otherOrKeyword=${otherOrKeyword}&notKeyword=${notKeyword}&page=${pageNumber}`)
+  getEntityByAnyKeywordNotOther(entity: string, params: string[], pageNumber: number): Observable<Page> {
+    return this.http.get(`${environment.API}/${entity}?orKeyword=${params[0]}&otherOrKeyword=${params[1]}&notKeyword=${params[2]}&page=${pageNumber}`)
       .map((res: Response) => {
         return new Page(res.json());
       })
       .catch((error: any) => Observable.throw(error.json()));
   }
 
-  getEntityByAnyKeywordAndOther(entity: string, orKeyword: string, otherOrKeyword: string, andKeyword: string,
-                                pageNumber: number): Observable<Page> {
-    return this.http.get(`${environment.API}/${entity}?orKeyword=${orKeyword}&otherOrKeyword=${otherOrKeyword}&andKeyword=${andKeyword}&page=${pageNumber}`)
+  getEntityByAnyKeywordAndOther(entity: string, params: string[], pageNumber: number): Observable<Page> {
+    return this.http.get(`${environment.API}/${entity}?orKeyword=${params[0]}&otherOrKeyword=${params[1]}&andKeyword=${params[2]}&page=${pageNumber}`)
       .map((res: Response) => {
         return new Page(res.json());
       })
