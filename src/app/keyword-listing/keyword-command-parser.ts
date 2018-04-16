670e585c4fbf3ceeae72ab11ca05e1acd71d4b34
Marc Ribalta Gene <mariva1995@gmail.com>
2018-04-16 12:49:02 +0200

    refactored the parsing methods

---
+++ src/app/keyword-listing/keyword-command-parser.ts
@@ -0,0 +1,57 @@
+
+
+
+class KeywordCommandParser {
+  private query = {params: [], sign: []};
+  private command: string;
+
+  createQuery(command: string): {}{
+    if(command.search(" AND | and | or | OR | NOT | not ") == -1){ //simple like
+      return this.constructSimpleLike(command);
+    }else if(command.search("\\(")){ // (x - y) - z
+      return this.constructComplexLike(command);
+    }else{ // x - y
+
+    }
+  }
+
+  private constructSimpleLike(command: string): {}{
+    this.query.sign.push("simple");
+    this.query.params.push(command);
+    return this.query;
+  }
+
+  private constructComplexLike(command: string): {}{
+    // parse the two lines
+    let {outerCondition, innerCondition} = this.getLineConditions();
+    let params = this.findCondition(innerCondition);
+    this.query.params.push(params[0]);
+    this.query.params.push(params[1]);
+    params = this.findCondition(outerCondition);
+    this.query.params.push(params.join(""));
+    return this.query;
+  }
+
+  private getLineConditions() {
+    let outerCondition = this.command.split(" ").join("");
+    let innerConditionStart = outerCondition.search("\\(");
+    let innerConditionEnd = outerCondition.search("\\)");
+    let innerCondition = outerCondition.substring(innerConditionStart, innerConditionEnd + 1);
+    outerCondition = outerCondition.replace(innerCondition, ""); // outter parentheses
+    innerCondition = innerCondition.substring(innerConditionStart + 1, innerConditionEnd); // inner parentheses
+    return {outerCondition, innerCondition};
+  }
+
+  private findCondition(condition: string) {
+    if (condition.search("AND|and") != -1) {
+      this.query.sign.push("AND");
+      return condition.split(/AND|and/);
+    } else if (condition.search("OR|or") != -1) {
+      this.query.sign.push("OR");
+      return condition.split(/OR|or/);
+    } else if (condition.search("NOT|not") != -1) {
+      this.query.sign.push("NOT");
+      return condition.split(/NOT|not/);
+    }
+  }
+}
--- src/app/keyword-listing/keyword-listing.component.ts
+++ src/app/keyword-listing/keyword-listing.component.ts
@@ -44,40 +44,7 @@ export class KeywordListingComponent implements OnInit {
       this.query.params.push(this.keyword);
       return this.keywordService.combinations.simple;
     }
-    // parse the two lines
-    let outerCondition = this.keyword.split(" ").join("");
-    let innerConditionStart = outerCondition.search("\\(");
-    let innerConditionEnd = outerCondition.search("\\)");
-    let innerCondition = outerCondition.substring(innerConditionStart, innerConditionEnd+1);
-    outerCondition = outerCondition.replace(innerCondition, ""); // outter parentheses
-    innerCondition = innerCondition.substring(innerConditionStart+1, innerConditionEnd); // inner parentheses
-    let s;
-    //find first condition
-    if(innerCondition.search("AND|and") != -1){
-      this.query.sign.push("AND");
-      s = innerCondition.split(/AND|and/);
-    }else if(innerCondition.search("OR|or") != -1){
-      this.query.sign.push("OR");
-      s = innerCondition.split(/OR|or/);
-    }else if(innerCondition.search("NOT|not") != -1){
-      this.query.sign.push("NOT");
-      s = innerCondition.split(/NOT|not/);
-    }
-    this.query.params.push(s[0]);
-    this.query.params.push(s[1]);
-    let x;
-    //find second condition
-    if(outerCondition.search("AND|and") != -1){
-      this.query.sign.push("AND");
-      x = outerCondition.split(/AND|and/).join("");
-    }else if(outerCondition.search("OR|or") != -1){
-      this.query.sign.push("OR");
-      x = outerCondition.split(/OR|or/).join("");
-    }else if(outerCondition.search("NOT|not") != -1){
-      this.query.sign.push("NOT");
-      x = outerCondition.split(/NOT|not/).join("");
-    }
-    this.query.params.push(x);
+
     //find method to be returned
     if(this.query.sign.length >= 2){
       console.log(this.query.sign[0] + " " + this.query.sign[1]);
