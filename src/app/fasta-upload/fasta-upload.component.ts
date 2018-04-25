093789d257d291b5a3c2b548c14c2e301e7ccd42
Marc Ribalta Gene <mariva1995@gmail.com>
2018-04-25 11:19:13 +0200

    now the sistem gives information to the client when a file is already uploaded

--- src/app/fasta-upload/fasta-upload.component.ts
+++ src/app/fasta-upload/fasta-upload.component.ts
@@ -4,6 +4,7 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {FileUploader} from 'ng2-file-upload';
 import {environment} from '../../environments/environment';
 import {AuthenticationBasicService} from "../login-basic/authentication-basic.service";
+import {ExperimentService} from "../experiment/experiment.service";
 
 @Component({
   selector: 'app-fasta-upload',
@@ -13,19 +14,32 @@ import {AuthenticationBasicService} from "../login-basic/authentication-basic.se
 })
 export class FastaUploadComponent implements OnInit {
 
+  warningMessage = "";
   fastaForm: FormGroup;
-  uploader = new FileUploader({url: `${environment.API}/upload/fasta`,
-    authToken: this.authentication.getCurrentUser().authorization});
+  uploader = new FileUploader({
+    url: `${environment.API}/upload/fasta`,
+    authToken: this.authentication.getCurrentUser().authorization
+  });
 
   constructor(private fb: FormBuilder,
               private router: Router,
-              private authentication: AuthenticationBasicService) {
+              private authentication: AuthenticationBasicService,
+              private experimentService: ExperimentService) {
     this.fastaForm = fb.group({
       'trinity-input': ['Fasta file', Validators.required]
     });
   }
 
   ngOnInit() {
+  }
+
+  onFileWanted() {
+    this.experimentService.getExperiment(this.uploader.queue.slice(-1)[0].file.name).subscribe(
+      (exp: string[]) => {
+        this.warningMessage = "This experiment already exists in the server. If you upload this file, the experiment will be reset and the information about sequences and families will be lost";
+      },
+      error => this.warningMessage = ""
+    );
   }
 
 }
