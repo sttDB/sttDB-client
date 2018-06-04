ae63b97da13248b3e150571871e9e398bda06fd8
Marc Ribalta Gene <mariva1995@gmail.com>
2018-06-04 12:18:07 +0200

    added the html and front-end part

--- src/app/go-upload/go-upload.component.ts
+++ src/app/go-upload/go-upload.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Page} from "../pager/page";
+import {Experiment} from "../experiment/experiment";
+import {ExperimentService} from "../experiment/experiment.service";
+import {FileItem, FileUploader, FileUploaderOptions} from "ng2-file-upload";
+import {environment} from "../../environments/environment";
+import {AuthenticationBasicService} from "../login-basic/authentication-basic.service";
 
 @Component({
   selector: 'app-go-upload',
@@ -7,9 +13,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoUploadComponent implements OnInit {
 
-  constructor() { }
+  uploader = new FileUploader({url: `${environment.API}/upload/go`,
+    authToken: this.authentication.getCurrentUser().authorization});
+  experiments: Experiment[];
+  selectedExperiment: string;
+  errorMessage: string;
+  warning;
 
-  ngOnInit() {
+  constructor(private experimentService: ExperimentService, private authentication: AuthenticationBasicService) {
   }
 
+  ngOnInit() {
+    this.experimentService.getExperimentsAsPage()
+      .subscribe(
+        (page: Page) => {
+          this.experiments = page.listOfElements;
+        },
+        error => this.errorMessage = <any>error.message);
+  }
+
+  upload(item: FileItem) {
+    if (this.selectedExperiment == null || this.selectedExperiment === 'none-selected') {
+      this.warning = "Please select an experiment";
+    } else {
+      this.warning = "";
+      let options: FileUploaderOptions = {};
+      options.headers = [{name: 'experiment', value: this.selectedExperiment}];
+      this.uploader.setOptions(options);
+      item.upload()
+    }
+  }
 }
