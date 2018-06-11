b107fbf59f34265a2cf9a4c472de5027ae9ffd72
Marc Ribalta Gene <mariva1995@gmail.com>
2018-06-11 10:30:49 +0200

    added keg files

---
+++ src/app/kegg-upload/kegg-upload.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kegg-upload',
+  templateUrl: './kegg-upload.component.html',
+  styleUrls: ['./kegg-upload.component.css']
+})
+export class KeggUploadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
